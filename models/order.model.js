acdd2d13893bc86b13e43b94c26339404871f052
B Srinivasa Ranganath <ranganathsrinivasa95@gmail.com>
2025-04-22 04:19:32 +0530

    Update order.model.js

--- models/order.model.js
+++ models/order.model.js
@@ -1,1 +1,22 @@
-export let orders = [];
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  items: [
+    {
+      title: String,
+      price: Number,
+    },
+  ],
+  address: String,
+  phone: String,
+  subtotal: String,
+  gst: String,
+  shipping: Number,
+  grandTotal: String,
+  deliveryDate: String,
+  deliveryPartner: String,
+  createdAt: { type: Date, default: Date.now }
+});
+
+export const Order = mongoose.model("Order", orderSchema);
