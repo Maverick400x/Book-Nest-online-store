8de22f116f922b5e2cac33bbdb9aa49945683f2d
B Srinivasa Ranganath <ranganathsrinivasa95@gmail.com>
2025-05-10 16:07:50 +0530

    Update user.model.js

--- models/user.model.js
+++ models/user.model.js
@@ -6,8 +6,12 @@ const userSchema = new mongoose.Schema({
   email:    { type: String, required: true, unique: true },
   password: { type: String, required: true },
   phone:    { type: String },     // Optional
-  address:  { type: String }      // Optional
+  address:  { type: String },     // Optional
+  otp: {
+    code:      { type: String },
+    expiresAt: { type: Date }
+  }
 });
 
-// Create and export the User model based on the schema
+// Create and export the User model
 export const User = mongoose.model("User", userSchema);
