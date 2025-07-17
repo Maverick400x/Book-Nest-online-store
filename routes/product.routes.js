a46e145633d3aca9861bf59fa48c5cee1b097fb3
B Srinivasa Ranganath <ranganathsrinivasa95@gmail.com>
2025-07-18 03:40:06 +0530

    Delete routes directory

--- routes/product.routes.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { getAllProducts } from "../controllers/product.controller.js";
-const router = express.Router();
-
-router.get("/", getAllProducts);
-
-export default router;
