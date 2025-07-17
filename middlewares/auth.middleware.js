11fc0ef80e37fadb542c7a59262c25dbbd2db2b9
B Srinivasa Ranganath <ranganathsrinivasa95@gmail.com>
2025-07-18 03:49:55 +0530

    Delete middlewares directory

--- middlewares/auth.middleware.js
+++
@@ -1,7 +0,0 @@
-export const authMiddleware = (req, res, next) => {
-    if (!req.session.user) {
-      return res.redirect("/users/login"); // Redirect to login page if not authenticated
-    }
-    next();
-  };
-  
