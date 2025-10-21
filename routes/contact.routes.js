16276af0a3e5804f8bdf872876d7968c7dbcdc1f
B Srinivasa Ranganath <ranganathsrinivasa95@gmail.com>
2025-10-21 13:17:12 +0530

    Delete routes directory

--- routes/contact.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { renderContactPage, handleContactForm } from "../controllers/contact.controller.js";
-
-const router = express.Router();
-
-router.get("/", renderContactPage);
-router.post("/", handleContactForm);
-
-export default router;
